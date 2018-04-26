610fecf3715c8c17dc9ffa675edc742592392b12
Richard-Ls <995837190@qq.com>
2018-04-26 18:15:24 +0800

    update the DDPG_update

--- DDPG/RL_brain_update.py
+++ DDPG/RL_brain_update.py
@@ -10,99 +10,133 @@ env:
 
 import numpy as np
 import tensorflow as tf
+import gym
 
 np.random.seed(1)
 tf.set_random_seed(1)
 
-GAMMA=0.9
+GAMMA = 0.9
 LR_A = 0.001
 LR_C = 0.001
 TAU = 0.01
-MEMORY_CAPACITY = 10000
+MEMORY_CAPACITY = 8000
 BATCH_SIZE = 32
 
 
-state_dim=10
-action_dim=3
-action_bound=5
+env=gym.make('Pendulum-v0')
+env=env.unwrapped
+env.seed(1)
+
+state_dim=env.observation_space.shape[0]
+action_dim=env.action_space.shape[0]
+action_bound=env.action_space.high
 
 class DDPG_update(object):
 
-    def __init__(self,a_dim,s_dim,a_bound):
-        self.memory=np.zeros((MEMORY_CAPACITY,2*s_dim+a_dim+1),dtype=np.float32)
-        self.pointer=0
+    def __init__(self, a_dim, s_dim, a_bound,):
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32)
+        self.pointer = 0
+        self.memory_full = False
+        self.sess = tf.Session()
+        self.a_replace_counter, self.c_replace_counter = 0, 0
 
-        self.sess=tf.Session()
+        self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound
+        self.S = tf.placeholder(tf.float32, [None, s_dim], 's')
+        self.S_ = tf.placeholder(tf.float32, [None, s_dim], 's_')
+        self.R = tf.placeholder(tf.float32, [None, 1], 'r')
 
-        self.a_dim,self.s_dim,self.a_bound=a_dim,s_dim,a_bound
-        self.s=tf.placeholder(tf.float32,[None,s_dim],'s')
-        self.s_=tf.placeholder(tf.float32,[None,s_dim],'s_')
-        self.r=tf.placeholder(tf.float32,[None,1],'r')
+        with tf.variable_scope('Actor'):
+            self.a = self._build_a(self.S, scope='eval', trainable=True)
+            a_ = self._build_a(self.S_, scope='target', trainable=False)
+        with tf.variable_scope('Critic'):
+            q = self._build_c(self.S, self.a, scope='eval', trainable=True)
+            q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
 
-        self.a=self._build_a(self.s,)
-        self.q=self._build_c(self.s,self.a)
+        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
+        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
+        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
+        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
 
-        a_params=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'Actor')
-        c_params=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'Critic')
-        ema=tf.train.ExponentialMovingAverage(decay=1-TAU)
+        self.soft_replace = [[tf.assign(ta, (1 - TAU) * ta + TAU * ea), tf.assign(tc, (1 - TAU) * tc + TAU * ec)]
+                             for ta, ea, tc, ec in zip(self.at_params, self.ae_params, self.ct_params, self.ce_params)]
 
-        self.target_update=[ema.apply(a_params),ema.apply(c_params)]
+        q_target = self.R + GAMMA * q_
 
-        def ema_getter(getter,name,*args,**kwargs):
-            return ema.average(getter(name,*args,**kwargs))
+        td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
+        self.ctrain = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=self.ce_params)
 
-        self.a_=self._build_a(self.s_,reuse=True,custom_getter=ema_getter)
-        self.q_=self._build_c(self.s_,self.a_,reuse=True,custom_getter=ema_getter)
-
-        self.a_loss=-tf.reduce_mean(self.q)
-        self.a_train=tf.train.AdamOptimizer(LR_A).minimize(self.a_loss,var_list=a_params)
-
-        with tf.control_dependencies(self.target_update):
-            q_target=self.r+GAMMA*self.q_
-            td_error=tf.losses.mean_squared_error(labels=q_target,predictions=self.q)
-            self.c_train=tf.train.AdamOptimizer(LR_C).minimize(td_error)
+        a_loss = - tf.reduce_mean(q)
+        self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
 
         self.sess.run(tf.global_variables_initializer())
 
-    def choose_action(self,s):
-        s=s[np.newaxis,:]
-        return self.sess.run(self.a,{self.s:s})[0]
-
-    def store_transition(self,s,a,r,s_):
-        transition=np.hstack((s,a,[r],s_))
-        index=self.pointer%MEMORY_CAPACITY
-        self.memory[index,:]=transition
-        self.pointer+=1
-
-    def _build_a(self,s,reuse=None,custom_getter=None):
-        trainable=True if reuse is None else False
-        with tf.variable_scope('Actor',custom_getter=custom_getter,reuse=reuse):
-            net=tf.layers.dense(s,30,tf.nn.relu,trainable=trainable)
-            a=tf.layers.dense(net,self.a_dim,tf.nn.tanh,trainable=trainable)
-        return tf.multiply(a,self.a_bound,name='scaled_a')
-
-    def _build_c(self,s,a,reuse=None,custom_getter=None):
-        trainable = True if reuse is None else False
-        with tf.variable_scope('Critic',reuse=reuse,custom_getter=custom_getter):
-            n_ll=30
-            w1_s=tf.get_variable('w1_s',[self.s_dim,n_ll],trainable=trainable)
-            w1_a=tf.get_variable('w1_a',[self.a_dim,n_ll],trainable=trainable)
-            b=tf.get_variable('b',[1,n_ll],trainable=trainable)
-            net=tf.matmul(s,w1_s)+tf.matmul(a,w1_a)+b
-            q=tf.layers.dense(net,1,trainable=trainable)
-        return q
+    def choose_action(self, s):
+        return self.sess.run(self.a, {self.S: s[np.newaxis, :]})[0]
 
     def learn(self):
-        indices=np.random.choice(MEMORY_CAPACITY,size=BATCH_SIZE)
-        bm=self.memory[indices,:]
 
-        bs=bm[:,:self.s_dim]
-        ba=bm[:,self.s_dim:self.s_dim+self.a_dim]
-        br=bm[:,self.s_dim+self.a_dim]
-        bs_=bm[:,-self.s_dim:]
+        self.sess.run(self.soft_replace)
 
-        self.sess.run(self.a_train,{self.s:bs})
-        self.sess.run(self.c_train,{self.s:bs,self.a:ba,self.r:br,self.s_:bs_})
+        indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE)
+        bt = self.memory[indices, :]
+        bs = bt[:, :self.s_dim]
+        ba = bt[:, self.s_dim: self.s_dim + self.a_dim]
+        br = bt[:, -self.s_dim - 1: -self.s_dim]
+        bs_ = bt[:, -self.s_dim:]
+
+        self.sess.run(self.atrain, {self.S: bs})
+        self.sess.run(self.ctrain, {self.S: bs, self.a: ba, self.R: br, self.S_: bs_})
+
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, a, [r], s_))
+        index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
+        self.memory[index, :] = transition
+        self.pointer += 1
+        if self.pointer > MEMORY_CAPACITY:      # indicator for learning
+            self.memory_full = True
+
+    def _build_a(self, s, scope, trainable):
+        with tf.variable_scope(scope):
+            net = tf.layers.dense(s, 300, activation=tf.nn.relu, name='l1', trainable=trainable)
+            a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
+            return tf.multiply(a, self.a_bound, name='scaled_a')
+
+    def _build_c(self, s, a, scope, trainable):
+        with tf.variable_scope(scope):
+            n_l1 = 300
+            w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
+            w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
+            b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
+            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+            return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
+
+    def save(self):
+        saver = tf.train.Saver()
+        saver.save(self.sess, './params', write_meta_graph=False)
+
+    def restore(self):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, './params')
+
+ddpg=DDPG_update(action_dim,state_dim,action_bound)
+
+s=env.reset()
+var=3
+while True:
+    env.render()
+
+    a=ddpg.choose_action(s)
+    a=np.clip(np.random.normal(a,var),-2,2)
+    s_,r,done,info=env.step(a)
+    if r>-1:
+        r+=20
+    ddpg.store_transition(s,a,r/10,s_)
+
+    if ddpg.memory_full:
+        var*=0.9995
+        ddpg.learn()
+    s=s_
+
 
 
 
